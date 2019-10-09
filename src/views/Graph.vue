43ba9297987288e1b4e2e54519240ea61e8436b6
R-Von <778615648@qq.com>
2019-10-09 11:20:35 +0800

    chore:change data

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -14,37 +14,68 @@ export default {
   methods:{
     paintChart(){
       let $container = document.getElementById('container')
-      // let testData = {
-      //   nodes:[
-      //     {
-      //       id:'TestNodeA'
-      //     },{
-      //       id:'TestNodeB'
-      //     }
-      //   ],
-      //   links:[{
-      //     source:'TestNodeA',
-      //     target:'TestNodeB'
-      //   }]
-      // }
+      let testData = {
+        nodes:[
+          {
+            id:'TestNodeA'
+          },{
+            id:'TestNodeB'
+          },
+          {
+            id:'TestNodeC'
+          },
+          {
+            id:'TestNodeD'
+          },
+          {
+            id:'TestNodeE'
+          }
+        ],
+        links:[
+         {
+          source:'TestNodeA',
+          target:'TestNodeB'
+         },
+         {
+          source:'TestNodeC',
+          target:'TestNodeB'
+         },
+         {
+          source:'TestNodeA',
+          target:'TestNodeD'
+         },
+         {
+          source:'TestNodeD',
+          target:'TestNodeE'
+         },
+          {
+          source:'TestNodeE',
+          target:'TestNodeD'
+         }
+         ]
+      }
+      
+      let chart = new D3ForceGraph($container,testData, {
+          width: 1000,
+          height: 1000,
+          nodeSize: 1800,
+          arrowSize: 600
+      })
+
 
       // let $container = document.getElementById('container')
-      $container.innerText = '数据拉取中，请稍候……'
-      this.$http.get('https://api.myjson.com/bins/pqc3q').then(data => {
-        console.log(data)
-          $container.innerText = ''
-          let chart = new D3ForceGraph($container,data, {
-              width: 1000,
-              height: 1000,
-              nodeSize: 1800,
-              arrowSize: 600
-          })
-      })
-      // this.$http.get('https://api.myjson.com/bins/pqc3q').then(res=>{
-      //   console.log(res)
-      // }).catch(err=>{
-      //   console.log(err)
+      // $container.innerText = '数据拉取中，请稍候……'
+      // this.$http.get('https://api.myjson.com/bins/pqc3q').then(data => {
+      //   console.log(data)
+      //     $container.innerText = ''
+      //     let chart = new D3ForceGraph($container,data, {
+      //         width: 1000,
+      //         height: 1000,
+      //         nodeSize: 1800,
+      //         arrowSize: 600
+      //     })
       // })
+    
     }
   },
   mounted(){
